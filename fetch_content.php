4bb4e61c1c25807b83e431c7e4c4ae900777655b
Takeshi Ueda <uedatakeshi@gmail.com>
2024-06-02 18:20:09 +0900

    modify

--- fetch_content.php
+++ fetch_content.php
@@ -1,6 +1,22 @@
+
+<div>
+    <form id="searchForm">
+            <label for="name">名前:</label>
+            <input type="text" id="name" name="name">
+            <button type="button" onclick="search()">検索</button>
+        </form>
+    </div>
+    <div id="searchResults">
+        <!-- 検索結果の表がここに表示されます -->
+    </div>
+</div>
+
+
+
 <?php
 // 動的に生成するコンテンツ
 echo "<h2>PHPからの動的コンテンツ</h2>";
 echo "<p>ここにPHPで生成されたコンテンツが表示されます。</p>";
+echo "<p>ここで普通にpostしてしまうとモーダルを外れてしまう・・・なんで</p>";
 ?>
 
